2746ddec4d3c8c51646accf16f8d915fe051fafc
anwaar12345 <syedanwar016@gmail.com>
2020-03-10 07:29:04 -0700

    Done with projects and created api for login and tommorrow CRUD API is To be Done Insha Allah

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Hash;
+
+use Auth;
+
+class LoginController extends Controller
+{
+    public function sendResponse($result, $message)
+    {
+    	$response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+
+        return response()->json($response, 200);
+    }
+
+
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+
+
+        return response()->json($response, $code);
+    }
+
+
+    public function login(Request $request)
+    {
+        
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')->accessToken; 
+            $success['name'] =  $user->name;
+            $success['password'] = $user->password;
+            
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+
+    // public function print()
+    // {
+    //     # code...
+    //     echo "hello";
+    // }
+
+
+    //
+
+
+}
---
+++ app/Http/Resources/user.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class user extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->detail,
+            'password' => $this->password
+        ];
+       // return parent::toArray($request);
+    }
+}
