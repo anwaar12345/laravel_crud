9d999f1c60f159bd9ef907a8e55af81631e7085c
anwaar12345 <syedanwar016@gmail.com>
2020-03-18 14:29:23 +0500

    Added Throttle Package validation per minute 1 user to Laravel 7 Blog APP

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ use Illuminate\Http\Request;
 // Route::post('login', '');
 //group middleware for restricting API
 
-Route::post('login1','API\LoginController@login')->middleware('requestloggs');
+Route::post('login1','API\LoginController@login')->middleware('requestloggs','throttle:1,1');
 // Route::get('posts','API\PostController@index');
 
-Route::group(['middleware' => 'auth.jwt','requestloggs'], function (){
+Route::group(['middleware' => 'auth.jwt','requestloggs'], function (){ // two middleware auth.jwt for jwt token and requestloggs to save API loggs in DataBase
     Route::get('posts','API\PostController@index');
    // Route::Post('ceateposts','API\LoginController@ceateposts');    
 });
